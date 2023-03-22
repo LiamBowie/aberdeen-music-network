a318d86383139d5340e46bc6d525a449c9d7b5fa
Liam Bowie <liambowie16@gmail.com>
2023-03-22 23:29:53 +0000

    Implement login and register user handlers

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,7 +3,40 @@ import { Link } from 'react-router-dom';
 import "./login.css";
 
 function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
+    const login = async (event) => {
+        event.preventDefault();
+        const data = { username, password }
+        await fetch('http://localhost:4001/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+    }
+    const register = async (event) => {
+        event.preventDefault();
+        const data = { username, password }
+        await fetch('http://localhost:4001/register', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+    }
+    const getUser = async (event) => {
+        event.preventDefault();
+        await fetch('http://localhost:4001/user', {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json'
+            },
+        });
+    }
 
     return (
         <div className="screen">
@@ -11,14 +44,13 @@ function Login() {
                 <h1 className='title'>Login</h1>
                 <form>
                     <label htmlFor="username">Username</label>
-                    <input type="text" name='username' placeholder='Type your username' />
+                    <input type="text" name='username' placeholder='Type your username' onChange={e => setUsername(e.target.value)}/>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name='password' placeholder='Type your password' />
+                    <input type="password" name='password' placeholder='Type your password' onChange={e => setPassword(e.target.value)} />
                     <div className='submitButtons'>
-                        <Link to="/">
-                            <button>Login</button>
-                        </Link>
-                        <button>Register</button>
+                        <button onClick={login}>Login</button>
+                        <button onClick={register}>Register</button>
+                        <button onClick={getUser}>Log user</button>
                     </div>
                 </form>
                 <span>Or sign up using</span>
